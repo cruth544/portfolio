07cded64d58f3420b3a98e22a2b5734dd2a5523c
Chad Rutherford <c.ruth544@gmail.com>
2016-02-25 15:47:33 -0800

    ok... lets try bootstrap

--- js/services.js
+++ js/services.js
@@ -5,13 +5,29 @@ function ProjectService() {
   var service = {}
   service.projects = [
     {name: 'Geometry Wars',
-      github: 'https://github.com/cruth544/Geometry-Wars'},
-    {name: 'Greenit',
-      github: 'https://github.com/cruth544/greenit'},
+      link: 'http://cruth544.github.io/Geometry-Wars/',
+      github: 'https://github.com/cruth544/Geometry-Wars',
+      picture: '../assets/Greenit-Screen-Shot.png',
+      description: 'description',
+      technologies: ['JavaScript']},
+    {name: 'Greennit',
+      link: 'https://greenit-app.herokuapp.com/',
+      github: 'https://github.com/cruth544/greennit',
+      picture: '../assets/Greenit-Screen-Shot.png',
+      description: 'description',
+      technologies: ['Ruby on Rails', 'PostgreSql']},
     {name: 'After-Hours',
-      github: 'https://github.com/cruth544/after-hours'},
+      link: 'http://after-hours-now.herokuapp.com/',
+      github: 'https://github.com/cruth544/After-Hours',
+      picture: '../assets/After-Hours-ScreenShot.png',
+      description: 'description',
+      technologies: ['NodeJS', 'ExpressJS', 'MongoDB']},
     {name: 'Lip',
-      github: 'https://github.com/cruth544/lip'}
+      link: 'https://d8bwwyct99aku.cloudfront.net/#/',
+      picture: '../assets/After-Hours-ScreenShot.png',
+      github: 'https://github.com/cruth544/Lip',
+      description: 'description',
+      technologies: ['AngularJS', 'NodeJS', 'ExpressJS', 'MongoDB', 'HTML5']}
   ]
 
   return service
