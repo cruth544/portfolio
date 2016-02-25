7692fbbff210adfa0ce3b41cfc5f431cabc08eeb
Chad Rutherford <c.ruth544@gmail.com>
2016-02-24 18:24:32 -0800

    one step closer

--- js/app.js
+++ js/app.js
@@ -1,13 +1,19 @@
-var app = angular.module('Portfolio')
-  .config(['$stateProvider', '$urlRouterProvider', '$httProvider', MainRouter])
+var app = angular.module('Portfolio', ['ui.router'])
+  .config(['$stateProvider', '$urlRouterProvider', '$httpProvider', MainRouter])
   .run(['$rootScope', '$state', '$location', runFunction])
 
-function MainRouter($stateProvider, $urlRouterProvider, $httProvider) {
+function MainRouter($stateProvider, $urlRouterProvider, $httpProvider) {
   $stateProvider
     .state('home', {
       url: '/',
-      templateUrl: '',
+      templateUrl: 'templates/main.html',
       params: null,
-      controller: ''
+      controller: 'MainCtrl'
     })
+
+  $urlRouterProvider.otherwise('/')
+}
+
+function runFunction($rootScope, $state, $location) {
+
 }
