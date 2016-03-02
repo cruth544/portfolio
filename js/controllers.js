b0ab70aad25d91a13d865b92e76fb205ae11815d
Chad Rutherford <c.ruth544@gmail.com>
2016-03-02 00:26:04 -0800

    game should be responsive

--- js/controllers.js
+++ js/controllers.js
@@ -2,4 +2,5 @@ app.controller('MainCtrl', ['$scope', 'Projects', MainCtrl])
 
 function MainCtrl ($scope, Projects) {
   $scope.projects = Projects.projects
+  $scope.badgeIcon = Projects.badgeIcon
 }
--- js/services.js
+++ js/services.js
@@ -3,6 +3,18 @@ app
 
 function ProjectService() {
   var service = {}
+  service.badgeIcon = {
+    HTML5: '../assets/HTML5_logo_and_wordmark.svg.png',
+    JavaScript: '../assets/js.png',
+    NodeJS: '../assets/nodejs.png',
+    ExpressJS: '../assets/express.png',
+    AngularJS: '../assets/angular.png',
+    MongoDB: '../assets/mongodb-logo-large.png',
+    Rails: '../assets/Ruby_on_Rails.svg.png',
+    Haml: '../assets/haml.png',
+    PostgreSql: '../assets/postgresql_logo-555px.png'
+  }
+
   service.projects = [
     {name: 'Geometry Wars',
       link: 'http://cruth544.github.io/Geometry-Wars/',
@@ -15,7 +27,7 @@ function ProjectService() {
       github: 'https://github.com/cruth544/greennit',
       picture: '../assets/Greenit-Screen-Shot.png',
       description: 'description',
-      technologies: ['Ruby on Rails', 'PostgreSql']},
+      technologies: ['Rails', 'PostgreSql', 'Haml']},
     {name: 'After-Hours',
       link: 'http://after-hours-now.herokuapp.com/',
       github: 'https://github.com/cruth544/After-Hours',
@@ -24,7 +36,7 @@ function ProjectService() {
       technologies: ['NodeJS', 'ExpressJS', 'MongoDB']},
     {name: 'Lip',
       link: 'https://d8bwwyct99aku.cloudfront.net/#/',
-      picture: '../assets/After-Hours-ScreenShot.png',
+      picture: '../assets/lipsyncwithus.png',
       github: 'https://github.com/cruth544/Lip',
       description: 'description',
       technologies: ['AngularJS', 'NodeJS', 'ExpressJS', 'MongoDB', 'HTML5']}
